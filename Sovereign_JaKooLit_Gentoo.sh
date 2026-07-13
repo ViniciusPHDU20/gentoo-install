67d29ca3193ebfd803c8410adaf497745767f1d4
ViniciusPHDU20 <viniciusphdu@github.com>
2026-07-13 03:09:56 -0300

    Add X USE flag to libxkbcommon to satisfy qtbase[X?]

--- Sovereign_JaKooLit_Gentoo.sh
+++ Sovereign_JaKooLit_Gentoo.sh
@@ -73,6 +73,7 @@ kde-frameworks/kconfig qml dbus
 app-text/xmlto text
 media-libs/libglvnd X
 x11-libs/cairo X
+x11-libs/libxkbcommon X
 media-libs/freetype harfbuzz
 net-wireless/wpa_supplicant dbus
 sys-apps/systemd policykit
